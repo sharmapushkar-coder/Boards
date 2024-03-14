cd0f99151c68601cc5255d093732c203b25116de
Pushkar Sharma <80835222+sharmapushkar-coder@users.noreply.github.com>
2024-03-14 20:16:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 22, 2024 00:00:00").getTime();
+var countDownDate = new Date("March 12, 2024 01:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
